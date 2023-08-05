c07a1d1c8b31631b6ccf940d2a4225da63f330e9
Azeem <aijaza@vcu.edu>
2023-08-04 21:44:01 -0400

    test

--- pages/api/posts/deletePost.ts
+++ pages/api/posts/deletePost.ts
@@ -24,7 +24,7 @@ export default async function handler(
             })
             res.status(200).json(result)
         } catch (err) {
-            res.status(403).json({ err: 'An Error has occured while deleting your posts' })
+            res.status(403).json({ err: 'An Error has occured while deleting your posts.' })
         }
     }
 }
