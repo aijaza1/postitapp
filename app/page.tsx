36334d96175b77825477922c607b042959b0c5ec
Azeem <aijaza@vcu.edu>
2023-08-06 21:48:45 -0400

    updates

--- app/page.tsx
+++ app/page.tsx
@@ -1,40 +1,39 @@
-'use client'
+"use client";
 
-import axios from 'axios'
-import AddPost from './components/AddPost'
-import {useQuery} from '@tanstack/react-query'
-import Post from './components/Post'
-import { PostType } from './types/Posts'
+import axios from "axios";
+import AddPost from "./components/AddPost";
+import { useQuery } from "@tanstack/react-query";
+import Post from "./components/Post";
+import { PostType } from "./types/Posts";
 
 // fetch all posts
-const allPosts = async() => {
-  const response = await axios.get('/api/posts/getPosts')
-  return response.data
-}
-
+const allPosts = async () => {
+  const response = await axios.get("/api/posts/getPosts");
+  return response.data;
+};
 
 export default function Home() {
-  const {data, error, isLoading} = useQuery<PostType[]>({
+  const { data, error, isLoading } = useQuery<PostType[]>({
     queryFn: allPosts,
     // caching, keep all the posts
     queryKey: ["posts"],
-})
-if (error) return error
-if (isLoading) return "Loading..."
+  });
+  if (error) return error;
+  if (isLoading) return "Loading...";
 
   return (
     <main>
       <AddPost />
       {data?.map((post) => (
         <Post
-          comments={post.Comment} 
-          key={post.id} 
-          name={post.user.name} 
-          avatar={post.user.image} 
-          postTitle={post.title} 
+          comments={post.Comment}
+          key={post.id}
+          name={post.user.name}
+          avatar={post.user.image}
+          postTitle={post.title}
           id={post.id}
         />
       ))}
     </main>
-  )
+  );
 }
