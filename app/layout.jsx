868d742a452955a2c232a233d63fded6b17c899a
Azeem <aijaza@vcu.edu>
2023-08-10 12:54:45 -0400

    browser icon update

--- app/layout.jsx
+++ app/layout.jsx
@@ -12,8 +12,26 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="/postupyicon.png" />
         <title>Postupy</title>
+
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
       </head>
       <body className={`mx-4 md:mx-48 xl:mx-96 ${nobile.variable} bg-[#e9e2fb]`}>
         <QueryWrapper>
