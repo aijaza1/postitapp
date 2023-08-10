4c11c33ea37164978a9ea74bddd28540f0c88a72
Azeem <aijaza@vcu.edu>
2023-08-10 11:31:04 -0400

    metadata test

--- app/layout.jsx
+++ app/layout.jsx
@@ -3,24 +3,17 @@ import { Nobile } from 'next/font/google'
 import Nav from './Nav'
 import AuthContext from "./auth/AuthContext"
 import QueryWrapper from './QueryWrapper'
-import { MetadataProvider } from './MetadataContext';
+
 
 const nobile = Nobile({ subsets: ['latin'], weight: ['400'] })
 
 
 export default function RootLayout({ children }) {
-  const metadata = {
-    title: 'PostPals',
-    description: "Share What's on your mind",
-  };
-
-
-
   return (
-    <MetadataProvider metadata={metadata}>
     <html lang="en">
       <head>
         <link rel="shortcut icon" href="/icon.png" />
+        <title>PostPals</title>
       </head>
       <body className={`mx-4 md:mx-48 xl:mx-96 ${nobile.variable} bg-[#e9e2fb]`}>
         <QueryWrapper>
@@ -31,7 +24,5 @@ export default function RootLayout({ children }) {
         </QueryWrapper>
       </body>
     </html>
-    </MetadataProvider>
-
   )
 }
